7fd1515dab3d19b4af85579ab2373cc26b6acbb5
dmltdev <hone.domitoro@gmail.com>
2023-12-21 14:02:23 +0200

    chore(deps): migrate CJS to ESM

--- generateExports.js
+++ generateExports.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 
 function readFiles(dirName) {
   fs.readdir(dirName, (err, files) => {
