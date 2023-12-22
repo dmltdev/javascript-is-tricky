f45a789e46164470ca4e6c2e220e8722c039757a
dmltdev <hone.domitoro@gmail.com>
2023-12-22 11:50:55 +0200

    chore: remove unused utils/geolocation/index.js

--- utils/geolocation/index.js
+++
@@ -1,3 +0,0 @@
-export { default as calculateDistance } from "./geolocation\calculateDistance.js"; 
-export { default as index } from "./geolocation\index.js"; 
-export { default as sortPlacesByDistance } from "./geolocation\sortPlacesByDistance.js"; 
